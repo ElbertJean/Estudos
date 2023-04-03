2769ca6ef7313d67941f2e53744b664e23d812af
Elbert Jean <elbertjeandossantos@gmail.com>
2023-04-03 12:15:04 -0300

    atualizacao ex1

--- 2-javascript/ex1/script.js
+++ 2-javascript/ex1/script.js
@@ -1,13 +1,36 @@
-function enviar () {
+function enviar (event) {
     var formulario = document.getElementById("formulario");
+    var paragraph = document.getElementById("paragraph");
+    var tabela_imc = document.getElementById("tabelaIMC"); 
     
-    var quilos = Number(formulario.quilos.value);  
-    var metros = Number(formulario.metros.value);
-    var centimetros = Number(formulario.centimetros.value);
- 
-    var altura = (metros * 100 + centimetros) / 100;
-    
-    var imc = quilos / (altura * altura);
+    var quilos = Number(formulario.quilos.value);
+    if (isNaN(quilos) || quilos <=0 || quilos >300){
+        paragraph.innerHTML = "Por favor, insira um peso válido!";
+    } else {
+        
+        var metros = Number(formulario.metros.value);
+        if (isNaN(metros) || metros <=0 || metros >2) {
+            paragraph.innerHTML = "Por favor, insira uma altura válida";
+        } else {
 
-    formulario.resultado.value = imc.toFixed(2);
+            var centimetros = Number(formulario.centimetros.value);
+            if (isNaN(centimetros) || centimetros <=0 || centimetros > 99){
+                paragraph.innerHTML = "Por favor, insira o valor válido de centimetros";
+            } else {
+                
+                var altura = (metros * 100 + centimetros) / 100;
+                var imc = quilos / (altura * altura);
+            
+                formulario.resultado.value = imc.toFixed(2);
+                paragraph.innerHTML="";
+
+                tabela_imc.src ="./tabela-imc.png";
+
+            }
+        }
+    }
+}
+
+function reset() {
+    document.getElementById("formulario").reset();
 }
