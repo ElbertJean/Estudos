f2b1d15bcc70b12dd7c626791adba68ff1336c84
Elbert Jean <elbertjeandossantos@gmail.com>
2023-04-30 21:35:14 -0300

    testando comunicação pdo com pgslq

---
+++ 8-php/Udemy/4-projetoCadastroDeCD/postgresql/conexao.php
@@ -0,0 +1,34 @@
+<?php
+
+//constantes
+define('HOST', 'localhost');
+define('DBNAME', 'ej_informatica');
+define('USER', 'elbinho');
+define('PASSWORD', 'Dgs589*-+');
+
+class Conect {
+	// atributo estático para instanciar do PDO
+	private static $pdo;
+
+	public function __construct(){
+		//
+	} 
+
+	//metodo estático 
+
+	public static function getInstance(){
+		if (!isset(self::$pdo)) {
+			try{
+				$opcoes = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',PDO::ATTR_PERSISTENT => TRUE);
+
+				self::$pdo = new PDO("pgsql:host=" . HOST . "; dbname=" . DBNAME . ";", USER, PASSWORD, $opcoes);
+		
+				//echo "conectado ao banco de dados: " . DBNAME;
+				} catch (PDOException $e) {
+					print "Erro ao se conectar ao banco: " . $e->getMessage();
+				}
+			}
+
+		return self::$pdo;
+	}
+}
---
+++ 8-php/Udemy/4-projetoCadastroDeCD/postgresql/home.php
@@ -0,0 +1,79 @@
+<?php 
+
+    require_once "./conexao.php";
+    $pdo = Conect::getInstance();
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/bootstrap.css">
+        <title>Bert Mercearia</title>
+    </head>
+    <nav style="margin-top:20px">
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link" href="./home.php"><b>Bert Mercearia</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" style="margin-right:10px; margin-left:10px" href="./home.php"><b>Home</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="margin-right:10px" href="#"><b>Cadastrar produto</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="margin-right:10px" href="#"><b>Sair</b></a>
+            </li>
+        </ul>
+    </nav>
+    <hr>
+    <body>
+        <div class="container">
+            <h1>Bert Mercearia</h1><br><br>
+            <div class="col-sm-12">
+                <table class="table">
+                    <tr>
+                        <th>
+                            ID
+                        </th>
+                        <th>
+                            Nome
+                        </th>
+                        <th>
+                            Descrição
+                        </th>
+                        <th>
+                            Preço
+                        </th>
+                        <th>
+                            Quantidade em estoque
+                        </th>
+                    </tr>
+                    <?php   
+
+                        $sql = ('SELECT cod_produto, nome_produto, descricao, preco, qtde_estoque FROM produtos'); //prepara a consulta
+                        $statement = $pdo->prepare($sql); //prepara os dados coletados do sql
+                        $statement->execute(); //executa os dados coletados
+                        
+                        $produtos = $statement->fetchAll(PDO::FETCH_ASSOC); //extrair todos os dados e armazena o resultado na variável
+
+                        foreach ($produtos as $value) {
+                            echo "<tr>";
+                            echo "<td>" . $value['cod_produto'] . "</td>";
+                            echo "<td>" . $value['nome_produto'] . "</td>";
+                            echo "<td>" . $value['descricao'] . "</td>";
+                            echo "<td>" . $value['preco'] . '</td>';
+                            echo "<td style=\"text-align:center\">"  . $value['qtde_estoque'] . "</td>";
+                        //    echo '<td><a href="./editar.php?id=' . $value['cod_produto'] . '">Editar</a><td>';
+                            echo "</tr>";
+                        }
+                    ?>
+                </table>
+            </div>
+
+        </div>
+    </body>
+</html>
